25e368fb52f5eb058ae05df2c83e9f05576453dc
Mohamad Ali <mohamadaliazizi000@gmail.com>
2024-09-12 23:49:09 +0330

    main mobile no task situation is done

--- main.js
+++ main.js
@@ -1,46 +1,64 @@
-//hamburger menu opertaions
+// greetings
+const greetingElement = document.getElementById('usernameMobile');
+
+async function updateGreeting() {
+  try {
+    const response = await fetch('userData.json');
+    const data = await response.json();
+    const userName = data.person1.name;
+    greetingElement.innerHTML += `سلام ${userName}`;
+  } catch (error) {
+    console.error('Error fetching JSON data:', error);
+  }
+}
+updateGreeting();
+
+//calendar requirements
+const date = new Date();
+const todaysDateMobile = document.getElementById('todaysDateMobile');
+todaysDateMobile.innerHTML += new Intl.DateTimeFormat('fa-IR', { dateStyle: 'medium' }).format(date);
+// const todaysDateDesktop = document.getElementById('todaysDateDesktop');
+// todaysDateDesktop.innerHTML += new Intl.DateTimeFormat('fa-IR', {dateStyle: 'medium'}).format(date);
+
+//hamburger menu operations
 const hamburger = document.getElementById('hamburger');
 const mobileMenu = document.getElementById('mobileMenu');
 const closeMenu = document.getElementById('closeMenu');
 const overlay = document.getElementById('overlay');
+
 const closeMobileMenu = () => {
-    mobileMenu.classList.add('translate-x-full');
-    overlay.classList.add('hidden');
+  mobileMenu.classList.add('translate-x-full');
+  overlay.classList.add('hidden');
 };
+
 hamburger.addEventListener('click', () => {
-    mobileMenu.classList.remove('translate-x-full');
-    overlay.classList.remove('hidden');
+  mobileMenu.classList.remove('translate-x-full');
+  overlay.classList.remove('hidden');
 });
+
 closeMenu.addEventListener('click', closeMobileMenu);
 overlay.addEventListener('click', closeMobileMenu);
 
 
+//toggle dark or light mode ------------------------------
+// const toggle = document.getElementById('darkModeToggle');
+// if (localStorage.getItem('theme') === 'dark') {
+//   document.documentElement.classList.add('dark');
+//   toggle.checked = true;
+// }
+// toggle.addEventListener('change', function () {
+//   if (toggle.checked) {
+//     document.documentElement.classList.add('dark');
+//     localStorage.setItem('theme', 'dark');
+//   } else {
+//     document.documentElement.classList.remove('dark');
+//     localStorage.setItem('theme', 'light');
+//   }
+// });
+// ---------------------------------------------------------
 
+//creating a new task
+const creatingNewTodo = document.getElementById('newtodo');
+creatingNewTodo.addEventListener('click', () => {
 
-const themeToggle = document.getElementById('theme-toggle');
-const themeIcon = document.getElementById('theme-icon');
-const sunIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" class="w-6 h-6"><path d="M12 3v2m0 14v2m7.071-9.071l-1.414 1.414m-9.9 9.9l1.414-1.414M21 12h-2M5 12H3m14.071 9.071l-1.414-1.414m-9.9-9.9l1.414-1.414m1.414 9.9l-1.414-1.414M12 5.828a6.172 6.172 0 016.172 6.172A6.172 6.172 0 0112 18.172 6.172 6.172 0 015.828 12 6.172 6.172 0 0112 5.828z" /></svg>';
-const moonIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" class="w-6 h-6"><path d="M21.72 15.61A9 9 0 018.39 2.28a7 7 0 100 14 7 7 0 0010.33 6.61A9 9 0 0021.72 15.61z"/></svg>';
-const toggleTheme = () => {
-  if (document.documentElement.classList.contains('dark')) {
-    document.documentElement.classList.remove('dark');
-    themeIcon.innerHTML = moonIcon;
-    localStorage.setItem('theme', 'light');
-  } else {
-    document.documentElement.classList.add('dark');
-    themeIcon.innerHTML = sunIcon;
-    localStorage.setItem('theme', 'dark');
-  }
-};
-const setInitialTheme = () => {
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'dark') {
-    document.documentElement.classList.add('dark');
-    themeIcon.innerHTML = sunIcon;
-  } else {
-    document.documentElement.classList.remove('dark');
-    themeIcon.innerHTML = moonIcon;
-  }
-};
-themeToggle.addEventListener('click', toggleTheme);
-setInitialTheme();
+});
